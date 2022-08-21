98cf8e839ebb3ba048a3a80ef67a6deab6d59fa6
MD. Arman Islam <armaanislam84@gmail.com>
2022-08-21 11:13:54 +0600

    Application User (In Memory User Details Manager)

--- src/main/java/com/armaan/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/armaan/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.armaan.springsecuritydemo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -12,7 +17,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     // By default, we get form based authentication; Now we are implementing Basic Authentication
     // There is no way to log out from basic authentication
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception { // Method for defining Basic Auth
         http
                 .authorizeRequests() // We must authorize requests
                 .antMatchers("/", "index", "/css/*", "/js/*") // White listing some URLs that we don't need to sign to view
@@ -21,5 +26,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated() // Any request must be authenticated; User must provide details
                 .and()
                 .httpBasic(); // Basic authentication mechanism that is being followed to authenticate user identity
+    }
+
+    @Override
+    @Bean // To instantiate this method
+    protected UserDetailsService userDetailsService() { // Method for defining Application User
+        UserDetails armaanUser = User.builder() // User from Spring Framework Security
+                .username("armaan")
+                .password("123")
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(
+                armaanUser
+        );
     }
 }
